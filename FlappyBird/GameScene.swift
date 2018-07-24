a6c3c3fdd317f5c60640a9565bc0636f8a69b35d
cygcyg <satoshi_ohkawa@oizu.co.jp>
2018-07-24 23:28:58 +0900

    とりあえず完成

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import  AVFoundation
 
 class GameScene: SKScene,SKPhysicsContactDelegate{
     
@@ -14,6 +15,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
     var wallNode:SKNode!
     var bird:SKSpriteNode!
     var item:SKNode!
+    var audioPlayer:AVAudioPlayer!
     
     // 衝突判定カテゴリー ↓追加
     let birdCategory: UInt32 = 1 << 0       // 0...00001
@@ -21,6 +23,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
     let wallCategory: UInt32 = 1 << 2       // 0...00100
     let scoreCategory: UInt32 = 1 << 3      // 0...01000
     let itemCategory: UInt32 = 1 << 4
+
     
     // スコア
     var score = 0
@@ -42,6 +45,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
         
         wallNode = SKNode()
         addChild(wallNode)
+
         
         // 各種スプライトを生成する処理をメソッドに分割
         setupGround()
@@ -260,7 +264,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
         itemTexture.filteringMode = .linear
         
         item = SKSpriteNode(texture: itemTexture)
-        item.position = CGPoint(x: self.size.width*rand, y: self.size.height*rand)
+        item.position = CGPoint(x: self.size.width, y: self.size.height*rand)
         item.physicsBody = SKPhysicsBody(rectangleOf: itemTexture.size())
         item.physicsBody?.categoryBitMask = itemCategory
         item.physicsBody?.collisionBitMask = birdCategory
@@ -272,6 +276,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
         let moveItemRight = SKAction.moveBy(x: -self.size.width , y: 0, duration: 10.0)
         let moveItemLeft = SKAction.moveBy(x: self.size.width, y: 0, duration: 10.0)
         
+        //画面内を往復させる
         let repeatScroll = SKAction.repeatForever(SKAction.sequence([moveItemRight,moveItemLeft]))
         
         item.run(repeatScroll)
@@ -355,17 +360,34 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
             (contact.bodyB.categoryBitMask & itemCategory) == itemCategory {
             //鳥とアイテムが衝突
             
+            //SKAudioNode版
+//            let music = SKAudioNode(fileNamed: "coin08.mp3")
+//            music.autoplayLooped = false
+//            self.run(SKAction.sequence(
+//                [SKAction.run
+//                    {music.run(SKAction.play())}
+//                ])
+//            )
+//            self.addChild(music)
+            
+            //AVAudioPlayer版
+            let audioPath = NSURL(fileURLWithPath: Bundle.main.path(forResource: "coin08", ofType: "mp3")!)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: audioPath as URL)
+                audioPlayer.play()
+            }
+            catch {
+
+            }
+
             print("ItemGet")
             itemScore += 1
             itemScoreLabelNode.text = "ItemScore:\(itemScore)"
-            item.removeFromParent()
+            self.removeChildren(in:[item])
             
             setupItem()
             
         }
-            
-            
-            
         else {
             // 壁か地面と衝突した
             print("GameOver")
